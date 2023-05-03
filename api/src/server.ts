9a9c3bbdd54711f4dfec302cc325f4c2ff9ef669
David Lima <antdavidlima@gmail.com>
2023-05-03 11:35:08 -0300

    chore(api): configura eslint

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,6 @@
-import { app } from "./app";
+import { app } from './app';
 
 app.listen({
   host: '0.0.0.0',
   port: 3333,
-}).then(() => console.log("🧔 Server running!"));
+}).then(() => console.log('🧔 Server running!'));
